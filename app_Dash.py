bf78dd1520debd593c787ec318f948f24a0ac8f4
Jason Young <jyoung367@gatech.edu>
2023-11-26 20:59:04 -0800

    updating app_Dash.py with top10 table, better formatting, relevant hover-over data, and sentiment score color-coding

--- app_Dash.py
+++ app_Dash.py
@@ -1,46 +1,80 @@
-#from flask import Flask, render_template, request
-#import json
 import pandas as pd
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
 
 
 app = Dash(__name__)
 
+
+# Load and format the data -------------------------------------------------------------
+
 df_reviews = pd.read_csv("Data/philly_reviews_asba.csv")
-#df_reviews['date'] = pd.to_datetime(df_reviews['date'], format='%B %d, %Y')
 df_businesses = df_reviews.iloc[:, 1:15].drop_duplicates()
+df_top10 = pd.read_csv("Data/top_10_coffee_shops.csv")
+bi_grams_results = pd.read_csv('Data/top_10_bigrams.csv')
+bi_grams_results = bi_grams_results.iloc[:, 1:]
+# join the composite (average) sentiment for each business
+df_temporary = df_reviews[['business_id', 'pss']].groupby('business_id').mean().reset_index()
+df_businesses = pd.merge(df_businesses, df_temporary[['business_id','pss']],on='business_id', how='left')
 
-
-#map_fig = go.Figure(data=go.Scattergeo(
-#        lon = df_businesses['longitude'],
-#        lat = df_businesses['latitude'],
-#        text = df_businesses['name'],
-#        mode = 'markers',
-#        marker_color = df_businesses['stars_x'],
-#        ))
-
-map_fig = px.scatter_mapbox(df_businesses, lat="latitude", lon="longitude", # hover_name="pss", hover_data=["State", "Population"],
-                        color_discrete_sequence=["blue"], zoom=10, height=500)
-
-map_fig.update_layout(mapbox_style="open-street-map")
-#map_fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-#map_fig.update_layout(mapbox_bounds={"west": -180, "east": -50, "south": 20, "north": 90})
+# -----------------------------------------------------------------------------------------
 
 
 
-#map_fig.update_layout(title = 'Coffee Shops in Philadelphia <br>(hover for more info)', 
-#    geo=dict(showland=True), autosize=True, width=1000, height=500)
+# Create the geographic map, with each business as one point on the map
+map_fig = px.scatter_mapbox(df_businesses, lat = 'latitude', lon = 'longitude',
+                        #radius = 30,
+                        center = dict(lat = 39.99, lon = -75.1), # manual- Philidelphia
+                        zoom = 10,
+                        mapbox_style = 'carto-positron', # this lowkey map style lets our color scheme stand out more
+                        size_max = 20,
+                        size = [70]*len(df_businesses), #each point is size 70
+                        color = 'pss', # color based on avg of sentiment scores
+                        color_continuous_scale = 'tealrose_r',
+                        opacity=1,
+                        width=1000,
+                        height=500,
+                        hover_name='name',
+                        hover_data= {
+                            #"size": False,   
+                            "latitude": False,
+                            "longitude": False,
+                            "address": True,
+                            #"attributes": True
+                        },
+                        labels={'pss': 'avg positive sentiment'}
+                        )
 
 
+# app layout including other visualizations
 app.layout = html.Div([
     html.H1(children='Coffee Re-Brewer', style={'textAlign':'center'}),
-    dcc.Dropdown(df_reviews.name.unique(), 'Vineyards Cafe', id='dropdown-selection'),
-    dcc.Graph(id='graph-content'),
-    dcc.Graph(figure=map_fig)
 
+    html.Div([
+
+        html.Div([
+            html.H2(children='Top 10 Coffee Shops by Sentiment Analysis'),
+            # The top 10 coffee shops
+            dash_table.DataTable(df_top10.to_dict('records'), [{"name": i, "id": i} for i in df_top10[['name', 'composite_score']]])
+            ], style={'width': '30%', 'float': 'left', 'margin-right': '5%', 'margin-top': '0%'}),
+
+        html.Div([
+            html.H2(children='The Landscape of Coffee Quality'),
+                # put the geo-map in the html structure
+                dcc.Graph(figure=map_fig)
+            ], style={'width': '55%', 'float': 'right', 'margin-left': '5%', 'margin-top': '0%'}),
+
+    ], style={'width': '65%', 'float': 'left'}),
+
+
+    html.Div([
+        html.H2(children='Coffee Sentiment Over Time'),
+        # dropdown of sentiment over time -- see update_graph() below
+        dcc.Dropdown(df_reviews.name.unique(), 'Vineyards Cafe', id='dropdown-selection'),
+        dcc.Graph(id='graph-content'),
+        ], style={'width': '65%', 'float': 'left'}),
 ])
 
 @callback(
@@ -49,7 +83,11 @@ app.layout = html.Div([
 )
 def update_graph(value):
     dff = df_reviews[df_reviews.name==value]
-    return px.scatter(dff, x='review_date', y='pss', color='name', title='Coffee Sentiment over Time')
+    return px.scatter(dff, x='review_date', y='pss', color=dff['pss'], color_continuous_scale='tealrose_r',#['red', 'green'],
+                      hover_name='review_text', facet_col_wrap=30,
+                      size = [30]*len(dff),
+                      labels={'review_date':'date of customer review', 'pss': 'coffee sentiment score'}
+                      )
 
 if __name__ == '__main__':
     app.run(debug=True)
